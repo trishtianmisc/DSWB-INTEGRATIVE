2b1b5cdec9ec9a15c5335ce4bbf011a70153d319
WhyH3r <wolverineneilrondina@gmail.com>
2025-05-25 22:18:52 +0800

    Add files via upload

---
+++ EventsPage/database.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";   
+$username = "root";         
+$password = "";           
+$dbname = "events";     
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} else {
+  echo "Connected successfully!";
+}
+?>
